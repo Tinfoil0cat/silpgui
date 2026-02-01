07e904a58dc6bd82d748343ef8aadcf31939a810
Just your daily guy <currycat08@gmail.com>
2026-02-01 15:23:53 -0500

    Replace my_hat_block with scratch_hat block

--- blocks.js
+++ blocks.js
@@ -1,20 +1,10 @@
-// The Hat Block
-Blockly.Blocks['my_hat_block'] = {
+Blockly.Blocks['scratch_hat'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("when clicked");
-    this.setNextStatement(true, null); // Only a bottom notch
-    this.setColour(60);
-  }
-};
-
-// A normal block to snap under it
-Blockly.Blocks['my_action_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("do something cool");
-    this.setPreviousStatement(true, null);
+        .appendField("when green flag clicked");
     this.setNextStatement(true, null);
-    this.setColour(230);
+    // Explicitly setting previous to null helps the renderer identify it as a top-level block
+    this.setPreviousStatement(false); 
+    this.setColour(60);
   }
 };
