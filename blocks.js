be6d6b172646ad87ef31496dfe4e89a38733ac45
Just your daily guy <currycat08@gmail.com>
2026-02-01 15:10:58 -0500

    Refactor Blockly blocks for event and action

--- blocks.js
+++ blocks.js
@@ -1,34 +1,23 @@
-// Define a custom "Statement" block (has notches)
+// 1. The Hat Block (Event)
+Blockly.Blocks['event_when_run'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("when program starts");
+    this.setNextStatement(true, null); // Has a notch below
+    this.setColour(60);                // Scratch gold
+    this.setTooltip("The starting point.");
+  }
+};
+
+// 2. A Standard Action Block (Statement)
 Blockly.Blocks['my_custom_action'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("perform action")
-        .appendField(new Blockly.FieldTextInput("default"), "NAME");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
+        .appendField("move")
+        .appendField(new Blockly.FieldNumber(10), "STEPS")
+        .appendField("steps");
+    this.setPreviousStatement(true, null); // Notch above
+    this.setNextStatement(true, null);     // Notch below
     this.setColour(230);
-    this.setTooltip("Does something cool.");
-  }
-};
-Blockly.Blocks['flagtap'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("when green flag tapped");
-    
-    // In Zelos, having a Next but NO Previous 
-    // automatically renders the "Hat" shape.
-    this.setNextStatement(true, null); 
-    
-    this.setColour(60); // Scratch-style yellow/gold
-    this.setTooltip("Starts the script.");
-  }
-};
-// Define a custom "Reporter" block (rounded in Zelos)
-Blockly.Blocks['my_custom_value'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("get sensor value");
-    this.setOutput(true, "Number");
-    this.setColour(160);
   }
 };
