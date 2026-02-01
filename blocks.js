3f2763a8947d3f782eccad3b73e438598db062a5
Just your daily guy <currycat08@gmail.com>
2026-02-01 15:19:45 -0500

    Refactor Blockly blocks for custom actions

--- blocks.js
+++ blocks.js
@@ -1,23 +1,20 @@
-// 1. The Hat Block (Event)
-Blockly.Blocks['event_when_run'] = {
+// The Hat Block
+Blockly.Blocks['my_hat_block'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("when program starts");
-    this.setNextStatement(true, null); // Has a notch below
-    this.setColour(60);                // Scratch gold
-    this.setTooltip("The starting point.");
+        .appendField("when clicked");
+    this.setNextStatement(true, null); // Only a bottom notch
+    this.setColour(60);
   }
 };
 
-// 2. A Standard Action Block (Statement)
-Blockly.Blocks['my_custom_action'] = {
+// A normal block to snap under it
+Blockly.Blocks['my_action_block'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("move")
-        .appendField(new Blockly.FieldNumber(10), "STEPS")
-        .appendField("steps");
-    this.setPreviousStatement(true, null); // Notch above
-    this.setNextStatement(true, null);     // Notch below
+        .appendField("do something cool");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
     this.setColour(230);
   }
 };
