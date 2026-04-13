90cf5b7ff43409b2b0aac59ff8a54ba19a568e66
Just your daily guy <currycat08@gmail.com>
2026-04-13 12:14:10 -0700

    Replace 'when_flag_clicked' block with 'movesteps'

--- blocks.js
+++ blocks.js
@@ -1,23 +1,11 @@
-Blockly.Blocks['when_flag_clicked'] = {
+Blockly.Blocks['movesteps'] = {
   init: function() {
     this.appendDummyInput()
-      .appendField("when")
-      .appendField(new Blockly.FieldImage(
-        "https://scratch.mit.edu/static/assets/38aa57841cf7733951ec650117a3a992.svg",
-        24, 
-        24, 
-        "*" // Alt text (required for clean console)
-      ))
-      .appendField("clicked");
-
-    // "Hat" logic: No previous statement, yes next statement
-    this.setPreviousStatement(false); 
+        .appendField("perform action")
+        .appendField(new Blockly.FieldTextInput("default"), "NAME");
+    this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-
-    this.setColour("#FFBF00");
-    
-    // REMOVED: this.setStyle('hat_block'); 
-    // Reason: Unless you define a custom theme object with this name, 
-    // this line overrides your setColour and makes the block black/gray.
+    this.setColour(230);
+    this.setTooltip("Does something cool.");
   }
 };
