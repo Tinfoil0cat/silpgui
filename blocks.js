226cf6b6c6ee653eb37de1211ca86086be4109f9
Just your daily guy <currycat08@gmail.com>
2026-04-14 10:14:38 -0400

    Add 'movex' block for moving x by specified steps

--- blocks.js
+++ blocks.js
@@ -9,3 +9,14 @@ init: function() {
         this.setColour("#4C97FF");
   }
 };
+
+Blockly.Blocks['movex'] = {
+init: function() {
+        this.appendDummyInput()
+            .appendField("move x by")
+            .appendField(new Blockly.FieldNumber(10), "STEPS");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour("#4C97FF");
+  }
+};
