b52536ede6d6917e0f8994485f0e1e814f9316d7
Just your daily guy <currycat08@gmail.com>
2026-04-14 10:12:23 -0400

    Modify movesteps block to move a specified number of steps

--- blocks.js
+++ blocks.js
@@ -1,11 +1,11 @@
 Blockly.Blocks['movesteps'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("perform action")
-        .appendField(new Blockly.FieldTextInput("default"), "NAME");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
-    this.setTooltip("Does something cool.");
+init: function() {
+        this.appendDummyInput()
+            .appendField("move")
+            .appendField(new Blockly.FieldNumber(10), "STEPS")
+            .appendField("steps");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour("#4C97FF");
   }
 };
